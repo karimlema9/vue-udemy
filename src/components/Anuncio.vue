e2034726066071c7b06bd7f3106833ac6e9b62bd
Karim Lemaallam <karim@ALONSO.local>
2019-12-11 11:43:20 +0100

    Afegir asientos y transicio

--- src/components/Anuncio.vue
+++ src/components/Anuncio.vue
@@ -1,73 +1,63 @@
 <template>
-    <div v-once class="anuncio">
-        <span>{{presentaciones[indice] }}</span>
-    </div>
+  <div class="anuncio">
+    <transition name="fade"  mode="out-in">
+      <span :key="indice">{{presentaciones[indice] }}</span>
+    </transition>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            presentaciones: [
-                'karim - 15:30',
-                'Joker - 17:00',
-                'Avengers - 18:30',
-                'El Rey Leon - 22:00'
-            ],
-            indice: 0
-        }
-    },
-    methods: {
-        mostrarAnuncio() {
-            this.indice++
+  data() {
+    return {
+      presentaciones: [
+        "karim - 15:30",
+        "Joker - 17:00",
+        "Avengers - 18:30",
+        "El Rey Leon - 22:00"
+      ],
+      indice: 0
+    };
+  },
+  methods: {
+    mostrarAnuncio() {
+      this.indice++;
 
-            if(this.indice == this.presentaciones.length) {
-                this.indice  = 0
-                // this.$destroy()
-            }
-        }
-    },
-    beforeCreate() {
-            console.log('beforeCreate')        
-    },
-    created() {
-        console.log('created')
-    },
-    beforeMount() {
-        console.log('beforeMount')
-    },
-    mounted() {
-        console.log('mounted')
-        setInterval(this.mostrarAnuncio, 2000)
-        this.mostrarAnuncio()
-    },
-    beforeUpdated() {
-        console.log('beforeUpdated')
-    },
-    updated() {
-        console.log('updated')
-    },
-    beforeDestroy() {
-        console.log('beforeDestroy')
-    },
-    destroyed() {
-        console.log('destroyed')
+      if (this.indice == this.presentaciones.length) {
+        this.indice = 0;
+      }
     }
-}
+  },
+  mounted() {
+    setInterval(this.mostrarAnuncio, 3000);
+  }
+};
 </script>
 
 <style>
 .anuncio {
-    margin: 10px;
-    font-size: 2rem;
+  margin: 10px;
+  font-size: 2.2rem;
 
-    border-top-color: white;
-    border-top-width: 1px;
-    border-top-style: solid;
+  border-top-color: white;
+  border-top-width: 1px;
+  border-top-style: solid;
 
-    border-bottom-color: white;
-    border-bottom-width: 1px;
-    border-bottom-style: solid;
+  border-bottom-color: white;
+  border-bottom-width: 1px;
+  border-bottom-style: solid;
+}
 
+.fade-enter {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: opacity 1s ease-out;
+}
+
+.fade-leave-active {
+  transition: opacity 0.6s ease-out;
+  opacity: 0;
 }
 </style>
--- src/components/Boletos.vue
+++ src/components/Boletos.vue
@@ -1,11 +1,41 @@
 <template>
-  <div class="seccion">
-    <div class="atributo">
-      <span class="boletos">{{boletos}}</span>
-    </div>
-    <div class="atributo">
-      <button @click="actualizarCantidad(1)">+</button>
-      <button @click="actualizarCantidad(-1)">-</button>
+  <div>
+    <div class="seccion">
+      <div class="seccion">
+        <div v-if="!pago && boletos == 0" class="mensaje informacion">Selecciona la menos un boleto.</div>
+        <div
+          v-else-if="!pago && boletos > 0"
+          class="mensaje advertencia"
+        >Recuerda completar tu compra.</div>
+        <div v-else class="mensaje exito">¡Bienvenido!</div>
+        <div class="atributo">
+          <span>Asientos</span>
+        </div>
+        <div class="atributo">
+          <button @click="seleccionar(asiento, index)" v-for="(asiento, index) in asientos" :key="index">{{ asiento }}</button>
+        </div>
+      </div>
+      <div class="atributo">
+        <span>Boletos</span>
+      </div>
+      <div class="atributo">
+        <span class="boletos">{{boletos}}</span>
+      </div>
+      <div v-show="boletos > 0" class="atributo">
+        <span>Total: ${{ total }}</span>
+      </div>
+      <div class="atributo">
+          <button class="seleccion" @click="remover(asiento, index)" v-for="(asiento, index) in seleccionados" :key="index">{{ asiento }}</button>
+        </div>
+      <div v-if="!pago && boletos > 0" class="atributo">
+        <button @click="pago = true">Pagar</button>
+      </div>
+      <div v-if="pago" class="atributo">
+        <button @click="reiniciar">Reiniciar</button>
+      </div>
+      <div class="atributo">
+        <span class="comision" :class="classeComision">${{comision}}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -14,30 +44,106 @@
 export default {
   data() {
     return {
-      boletos: 0
+      comision: 0,
+      classeComision: "neutro",
+      pago: false,
+      asientos: [
+        'A1','A2','A3','A4','A5',
+        'B1','B2','B3','B4','B5'
+      ],
+      seleccionados: []
+    };
+  },
+  computed: {
+    boletos() {
+      return this.seleccionados.length
+    },
+    total() {
+      return this.boletos * 100;
     }
   },
   methods: {
-      actualizarCantidad(cantidad) {
-          this.boletos += cantidad
-
-          if (this.boletos > 10) {
-              this.boletos = 10
-          }
-
-          if (this.boletos < 0) {
-              this.boletos = 0
-          }
+    seleccionar(asiento, index) {
+      this.asientos.splice(index, 1)
+      this.seleccionados.push(asiento)
+      this.seleccionados.sort()
+    },
+    remover(asiento, index) {
+      this.seleccionados.splice(index, 1)
+      this.asientos.push(asiento)
+      this.asientos.sort()
+    },
+    reiniciar() {
+      this.pago = false;
+      this.asientos.push(...this.seleccionados)
+      this.asientos.sort()
+      this.seleccionados = []
+      this.comision = 0;
+    }
+  },
+  watch: {
+    boletos(newValue, oldValue) {
+      if (newValue > oldValue) {
+        this.comision += 10;
+        this.classeComision = "incremento";
+      } else {
+        this.comision -= 15;
+        this.classeComision = "decremento";
       }
+
+      if (this.comision < 0) {
+        this.comision = 0;
+        this.classeComision = "neutro";
+      }
+    }
   }
 };
 </script>
 
 <style>
-
 .boletos {
-    font-size: 4rem;
-    font-weight: bold;
+  font-size: 4rem;
+  font-weight: bold;
 }
 
+.comision {
+  font-size: 3.2rem;
+}
+
+.neutro {
+  color: black;
+}
+
+.incremento {
+  color: green;
+}
+
+.decremento {
+  color: red;
+}
+
+.mensaje {
+  border-radius: 5px;
+  border-width: 4px;
+  border-style: dashed;
+  padding: 10px;
+  font-size: 2.1rem;
+  font-weight: bold;
+}
+
+.informacion {
+  border-color: blue;
+}
+
+.advertencia {
+  border-color: orange;
+}
+
+.exito {
+  border-color: green;
+}
+
+.seleccion {
+  background-color: #1dca1d;
+}
 </style>
